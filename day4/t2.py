bb7686859be35065f55ebc8a87c7215914373e39
Zhaopeng Tao <38628261+Otsutsukii@users.noreply.github.com>
2021-12-04 14:42:16 +0100

    added numpy solution

--- day4/t2.py
+++ day4/t2.py
@@ -1,23 +1,149 @@
+from typing import BinaryIO
+import numpy as np
+# Bingo is played on a set of boards each consisting of a 5x5 grid of numbers. Numbers are chosen at random, and the chosen number is marked on all boards on which it appears. (Numbers may not appear on all boards.) If all numbers in any row or any column of a board are marked, that board wins. (Diagonals don't count.)
+
+# The submarine has a bingo subsystem to help passengers (currently, you and the giant squid) pass the time. It automatically generates a random order in which to draw numbers and a random set of boards (your puzzle input). For example:
+
+# 7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+# 22 13 17 11  0
+#  8  2 23  4 24
+# 21  9 14 16  7
+#  6 10  3 18  5
+#  1 12 20 15 19
+
+#  3 15  0  2 22
+#  9 18 13 17  5
+# 19  8  7 25 23
+# 20 11 10 24  4
+# 14 21 16 12  6
+
+# 14 21 17 24  4
+# 10 16 15  9 19
+# 18  8 23 26 20
+# 22 11 13  6  5
+#  2  0 12  3  7
+# After the first five numbers are drawn (7, 4, 9, 5, and 11), there are no winners, but the boards are marked as follows (shown here adjacent to each other to save space):
+
+# 22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+#  8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+# 21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+#  6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+#  1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+# After the next six numbers are drawn (17, 23, 2, 0, 14, and 21), there are still no winners:
+
+# 22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+#  8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+# 21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+#  6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+#  1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+# Finally, 24 is drawn:
+
+# 22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+#  8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+# 21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+#  6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+#  1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+# At this point, the third board wins because it has at least one complete row or column of marked numbers (in this case, the entire top row is marked: 14 21 17 24 4).
+
+# The score of the winning board can now be calculated. Start by finding the sum of all unmarked numbers on that board; in this case, the sum is 188. Then, multiply that sum by the number that was just called when the board won, 24, to get the final score, 188 * 24 = 4512.
+
+# To guarantee victory against the giant squid, figure out which board will win first. What will your final score be if you choose that board?
 
 
 
+def solution1(line,data):
+#     find the first board that has a complete row or column
+    # load the matrixes using numpy, and for each number in line, find the board that has that number
+    # for each board, find the row or column that has that number
+    # set the board to the number of the row or column
+    winners = []
+    score = []
+    data = np.array(data)
+    marked = np.full(data.shape,-1.0)
+    for num in line:
+        # find the board that has the number
+        locations = np.where(data == num)
+        # find the row or column that has the number
+        for board,row,col in zip(*locations):
+            if board not in winners:
+                # set the board to the number of the row or column
+                marked[board,row,col] = np.nan
+                # find the row or col that does not countain nan 
+                # if the row or col is complete, then the board is a winner
+                if np.all(np.isnan(marked[board][row])) or np.all(np.isnan(marked[board].T[col]) ):
+                    winners.append(board)
+                    #compute score of winner
+                    #replace all nan value of the marked[board] with 0
+                    marked[board] = np.where(np.isnan(marked[board]),+0,marked[board])
+                    marked[board] = np.where(marked[board]==-1,1,marked[board])
+                    print(marked[board])
+                    score.append(np.sum(np.multiply(data[board],marked[board]))*num)
+    return score
 
+#-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+# --- Part Two ---
+# On the other hand, it might be wise to try a different strategy: let the giant squid win.
 
+# You aren't sure how many bingo boards a giant squid could play at once, so rather than waste time counting its arms, the safe thing to do is to figure out which board will win last and choose that one. That way, no matter which boards it picks, it will win for sure.
 
-def solution1(data):
+# In the above example, the second board is the last to win, which happens after 13 is eventually called and its middle column is completely marked. If you were to keep playing until this point, the second board would have a sum of unmarked numbers equal to 148 for a final score of 148 * 13 = 1924.
+
+# Figure out which board will win last. Once it wins, what would its final score be?
+
+def solution2(line,data):
     pass
+    
 
-
-def solution2(data):
-    pass
 
 def read_input(inputfile):
-    with open(inputfile, 'r') as f:
-        data = f.read().splitlines()
-    return data
+    """
+    7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+    22 13 17 11  0
+     8  2 23  4 24
+    21  9 14 16  7
+     6 10  3 18  5
+     1 12 20 15 19
+
+     3 15  0  2 22
+     9 18 13 17  5
+    19  8  7 25 23
+    20 11 10 24  4
+    14 21 16 12  6
+
+    14 21 17 24  4
+    10 16 15  9 19
+    18  8 23 26 20
+    22 11 13  6  5
+     2  0 12  3  7
+    
+    Read the below into a list of interger from the list line
+    Read matrix separated by an empty line
+    """
+    # read the first line of the inputfile into a list of integer
+    with open(inputfile) as f:
+        l = f.readline().split(',')
+        l = [int(x) for x in l]
+        # read following lines into a list of matrix of integer, matrixes are split by the empty line
+        data = []
+        for line in f:
+            if line == "\n":
+                data.append([])
+                continue
+            line = line.split()
+            line = [int(x) for x in line]
+            data[-1].append(line)
+    return l,data
+    
+
+    
 
 if __name__ == '__main__':
     inputfile = 'input.txt'
     data = read_input(inputfile)
-    print(solution1(data))
-    print(solution2(data))
+    # print(data)
+    print(solution1(data[0],data[1]))
+    # print(solution2(data[0],data[1]))
+    
